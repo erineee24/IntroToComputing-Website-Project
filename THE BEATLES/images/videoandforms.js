1912bc614e7f3e7fbb1458c5f1bb2c8c13d3595c
erineee24 <s.cegomez@usm.edu.ph>
2024-12-19 23:34:51 -0800

    IntroToComputing-Website-Project

---
+++ THE BEATLES/images/videoandforms.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Get the video element and play/pause buttons
+    const video = document.getElementById('videoPlayer');
+    const playButton = document.getElementById('playButton');
+    const pauseButton = document.getElementById('pauseButton');
